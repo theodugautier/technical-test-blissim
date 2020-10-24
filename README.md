2a2ba75fdb016c328099d5d7a856df67adb65a84
Theo Dugautier <theo.dugautier@protonmail.com>
2020-10-25 00:31:29 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Test Technique Blissim
+
+## Sommaire
+
+````
+docker-compose up -d
+````
+
+Pour l'api : localhost:4000/index.php
+Pour l'app : localhost:3000
---
+++ app/components/boutique/ProductCard.js
@@ -0,0 +1,79 @@
+import {
+    Card,
+    CardContent,
+    CardActions,
+    CardMedia,
+    Typography,
+    Button,
+    withStyles,
+    IconButton
+} from '@material-ui/core'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import {useContext} from "react";
+import GlobalContext from "../../state/global-context";
+
+const useStyles = theme => ({
+    root: {
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+    },
+    content: {
+        width: "100%",
+    },
+    thumbnailContainer: {
+        padding: theme.spacing(2),
+        textAlign: "cetner",
+    },
+    thumbnail: {
+        maxHeight: '170px',
+        width: "auto",
+        margin: "auto",
+    },
+    name: {
+        fontSize: '1rem',
+    }
+});
+
+const ProductCard = (props) => {
+    const {classes, product} = props
+    const context = useContext(GlobalContext);
+
+    const handleAddToCart = (e, product) => {
+        context.addProductToCart(product, context.pushObject('open_interstitial', true))
+    }
+
+    return (
+        <Card className={classes.root}>
+            <CardContent className={classes.content}>
+                <div className={classes.thumbnailContainer}>
+                    <CardMedia
+                        component="img"
+                        alt={product.title}
+                        image={product.image}
+                        className={classes.thumbnail}
+                        title="Contemplative Reptile"
+                    />
+                </div>
+                <Typography gutterBottom component="h2" className={classes.name}>
+                    {product.title}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {product.desc}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {product.price}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <IconButton onClick={e => handleAddToCart(e, product)}>
+                    <ShoppingBasketIcon color="secondary"/>
+                </IconButton>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default withStyles(useStyles)(ProductCard)

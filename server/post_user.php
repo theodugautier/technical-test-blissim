df55f4602c541b8581b814ba493eabbe8d06ac84
theodugautier <dugautiertheo@gmail.com>
2020-10-30 10:27:26 +0100

    fix(client): env variables

--- server/post_user.php
+++ server/post_user.php
@@ -8,39 +8,37 @@
   header('access-control-expose-headers: Set-Cookie');
   session_start();
   
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $rest_json = file_get_contents("php://input");
-    $_POST = json_decode($rest_json, true);
+  $rest_json = file_get_contents("php://input");
+  $_POST = json_decode($rest_json, true);
 
-    if( empty($_POST['name']) || empty($_POST['age']) ) {
-        http_response_code(400);
-        echo json_encode(
-            [
-                "message" => "age or name is not filled in",
-            ]
-        ); 
-        exit();
+  if( empty($_POST['name']) || empty($_POST['age']) ) {
+      http_response_code(400);
+      echo json_encode(
+          [
+              "message" => "age or name is not filled in",
+          ]
+      ); 
+      exit();
+  }
+
+  if ($_POST) {
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $newUser = new User($name, $age);
+
+    if (!isset($_SESSION["users"])) {
+      $_SESSION["users"] = array();
     }
 
-    if ($_POST) {
-      $name = $_POST['name'];
-      $age = $_POST['age'];
-      $newUser = new User($name, $age);
+    array_push($_SESSION["users"], ["name" => $newUser->getName(), "age" => $newUser->getAge()]);
 
-      if (!isset($_SESSION["users"])) {
-        $_SESSION["users"] = array();
-      }
-
-      array_push($_SESSION["users"], ["name" => $newUser->getName(), "age" => $newUser->getAge()]);
-
-      http_response_code(201);
-      echo json_encode($_SESSION["users"]);
-    } else {
-      http_response_code(500);
-      echo json_encode(
-        [
-          "message" => "There seems to have been an error."
-        ]
-      );
-    } 
-  }
+    http_response_code(201);
+    echo json_encode($_SESSION["users"]);
+  } else {
+    http_response_code(500);
+    echo json_encode(
+      [
+        "message" => "There seems to have been an error."
+      ]
+    );
+  } 
